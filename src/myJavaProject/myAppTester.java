657708994317e3b51a4b3b6f4fc20fd75903eac6
Darya <lapshinad@student.douglascollege.ca>
2020-06-09 18:17:55 -0700

    Adding a testMethod

--- src/myJavaProject/myAppTester.java
+++ src/myJavaProject/myAppTester.java
@@ -16,8 +16,7 @@ public class myAppTester {
 	  System.out.print("Reverse of the input number is:");
 	  reverseMethod(n);
 	  System.out.println();
-
-	  // add a line below to call your myTestMethod from here  then remove the block comment symbols  
+	  myTestMethod(n);
 
 	}
 	
@@ -33,15 +32,16 @@ public class myAppTester {
 	       }
 	   }
 
-
-	   /* 
-	       add your test method in this space then remove the block comment symbols
-	       public static void myTestMethod(...){
-
+	     
+	       public static void myTestMethod(int num){
+	    	   System.out.println("Printing from the number down:");
+	    	   for (int i = num; i>=0;i--) {
+	    		   System.out.println(i);
+	    	   }
             
 
 		   }
 	 
-	   */
+	   
 
 }
